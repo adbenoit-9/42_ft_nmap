c5450ef5938947662c396b2cdca9ebfd62f411ac
Votre Nom <lmariott@student.42.fr>
2022-08-19 09:02:47 +0200

    fix compilation

--- srcs/nmap/main.c
+++ srcs/nmap/main.c
@@ -16,6 +16,7 @@
 #include <pthread.h>
 # include <string.h>
 # include "ft_nmap_parsing.h"
+# include "ft_nmap_structs.h"
 # include "libft.h"
 
 //# include "ft_nmap_sendy.h"
@@ -28,8 +29,7 @@ static		uint8_t		buf[MAPPY_SIZE + SETY_SIZE];
 int main(int ac, char **av)
 {
 	int32_t 		r = RUNNY_OK;
-	uint8_t			*buf;
-	t_nmap_config	opt;
+	t_nmap_setting	opt;
 
 	if (buf == NULL)
 	{
@@ -38,22 +38,24 @@ int main(int ac, char **av)
 	if (r == RUNNY_OK)
 	{
 		/* Set buffer config and map */
-		opt.map = buf;
-		opt.config = &buf[MAPPY_SIZE];
+		opt.cross.map = buf;
+		opt.cross.config = &buf[MAPPY_SIZE];
 
 		/* Now fill opt with cmdline options */
-		opt = parsy_getopt(ac, av, opt);
+		//opt = parsy_getopt(ac, av, opt);
+	(void)ac;
+	(void)av;
 
 		/* Set opt tasks + elements_nb -> function nmap_config_branch_hook -> hooky ? */
-		opt.elements_nb = 0x42;
-		int i = 0;
-		while (j < opt.elements_nb)
+		opt.cross.nb_elements = 0x42;
+		uint32_t j = 0;
+		while (j < opt.cross.nb_elements)
 		{
 			/* Cmd target */
 			/* Hook target */
 		}
-		r = sety_root(&opt);
-		r = mappy(&opt);
+		r = sety_root((t_proto_root*)&opt);
+		r = mappy((t_proto_root*)&opt);
 
 //		for (uint8_t i = 0; i < 250; i++) {
 //			if (pthread_create(&threads[i], NULL, (void *)routine, opt) != 0)
