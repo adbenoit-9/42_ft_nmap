77deac1b38075c2886c29561f0e6a90a7e383e84
Votre Nom <lmariott@student.42.fr>
2022-08-21 15:46:46 +0200

    merge

--- srcs/nmap/mapy_hooks.c
+++ srcs/nmap/mapy_hooks.c
@@ -6,7 +6,7 @@
 /*   By: leon <lmariott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 15:28:22 by leon              #+#    #+#             */
-/*   Updated: 2022/08/21 15:33:07 by leon             ###   ########.fr       */
+/*   Updated: 2022/08/21 15:45:27 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int					print_all(uint8_t *buf, t_nmap_link *link, t_nmap_app *app, t_nmap_scan 
 	(void)app;
 	(void)scan;
 	fprintf(stderr, "%s:%d buf=%p\n\n", __func__, __LINE__, buf);
-	while (i < MAP_BLCK_SIZE)
+	while (i < scan->packet_length)
 	{
 		if (i % 32 == 0)
 		{
