a59a0550dcf3e61b356641d7373b0f926ab17944
adbenoit-9 <adeline.benoit@hotmail.com>
2022-08-23 13:44:26 +0200

    [ADD] define ipv6

--- srcs/buildy/buildy.h
+++ srcs/buildy/buildy.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   buildy.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leon <lmariott@student.42.fr>              +#+  +:+       +#+        */
+/*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 21:25:20 by leon              #+#    #+#             */
-/*   Updated: 2022/08/21 13:48:31 by leon             ###   ########.fr       */
+/*   Updated: 2022/08/23 13:43:36 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,25 @@
 
 #define BUILDY_OK	0
 #define BUILDY_ERROR -1
+
+# ifndef MAC
+
+#  define IPV6_VERSION            0x60
+#  define IPV6_VERSION_MASK       0xf0
+
+#  if BYTE_ORDER == BIG_ENDIAN
+#   define IPV6_FLOWINFO_MASK      0x0fffffff      /* flow info (28 bits) */
+#   define IPV6_FLOWLABEL_MASK     0x000fffff      /* flow label (20 bits) */
+#   define IPV6_FLOW_ECN_MASK      0x00300000      /* the 2 ECN bits */
+#  else
+#   if BYTE_ORDER == LITTLE_ENDIAN
+#    define IPV6_FLOWINFO_MASK      0xffffff0f      /* flow info (28 bits) */
+#    define IPV6_FLOWLABEL_MASK     0xffff0f00      /* flow label (20 bits) */
+#    define IPV6_FLOW_ECN_MASK      0x00003000      /* the 2 ECN bits */
+#   endif /* LITTLE_ENDIAN */
+#  endif
+
+# endif
 
 #define SELECT_HOOK_BUILDY(sockaddr, nd, hook) do { \
 if (((struct sockaddr_storage *)sockaddr).ss_family == AF_INET) { \
