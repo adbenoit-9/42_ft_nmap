188f252f74853e46de680c1f268b0feb1525ce53
adbenoit-9 <adeline.benoit@hotmail.com>
2022-08-25 16:24:48 +0200

    [ADD] thread & mutex [ERROR] data race due to buffer mutex (mutex.__sig = 0)

--- srcs/nmap/nmap_exey_hooks.c
+++ srcs/nmap/nmap_exey_hooks.c
@@ -3,41 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   nmap_exey_hooks.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leon <lmariott@student.42.fr>              +#+  +:+       +#+        */
+/*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 17:25:33 by leon              #+#    #+#             */
-/*   Updated: 2022/08/24 12:01:49 by leon             ###   ########.fr       */
+/*   Updated: 2022/08/25 16:20:21 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mapy.h"
+#include <pthread.h>
 
 //#define EXEY_DEBUG
 
 #define EXEY_OK 0
+#define EXEY_ERR -1
 
-#ifdef EXEY_DEBUG
 #include <stdio.h>
-#endif
+#include <string.h>
+#include <unistd.h>
 
 static const uint8_t default_execution_list[] = {0x00, 0x01, 0x02};
 
 int					nmap_init_exey(T_CLIENT_ROOT *root, t_exe *exe , t_blk *blk)
 {
-	uint32_t		i;
+	uint32_t	i;
+	int			ret;
 
 	(void)root;
-	(void)blk;
 	i = 0;
-	while (i < 2) //sizeof(default_execution_list))
+	ret = EXEY_ERR;
+	if ((ret = pthread_mutex_init(&exe->mutex, NULL)) != 0) {
+		dprintf(STDERR_FILENO, "pthread_mutex_init (exe): %s\n", strerror(ret));
+		ret = EXEY_ERR;
+	}
+	if (blk && (ret = pthread_mutex_init(&blk->mutex, NULL)) != 0) {
+		dprintf(STDERR_FILENO, "pthread_mutex_init (blk): %s\n", strerror(ret));
+		ret = EXEY_ERR;
+	}
+	while (ret == EXEY_OK && i < 2) //sizeof(default_execution_list))
 	{
 		exe->tasks[i] = default_execution_list[i] | EXEC_TODO_MSK;
 		exe->hook[i] = 0x00;
+
 #ifdef EXEY_DEBUG
 		fprintf(stderr, "%s:%d exe->tasks[%d] = %08x hook=%08x\n", __func__, __LINE__, i, exe->tasks[i], exe->hook[i]);
 #endif
 		i++;
 	}
+	return (ret);
+}
+
+int					nmap_clean_exey(T_CLIENT_ROOT *root, t_exe *exe , t_blk *blk)
+{
+	(void)root;
+	pthread_mutex_destroy(&exe->mutex);
+	pthread_mutex_destroy(&blk->mutex);
 	return (EXEY_OK);
 }
 
--- srcs/nmap/nmap_exey_hooks.h
+++ srcs/nmap/nmap_exey_hooks.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exey_hooks.h                                       :+:      :+:    :+:   */
+/*   nmap_exey_hooks.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leon <lmariott@student.42.fr>              +#+  +:+       +#+        */
+/*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 14:10:49 by leon              #+#    #+#             */
-/*   Updated: 2022/08/21 17:29:21 by leon             ###   ########.fr       */
+/*   Updated: 2022/08/25 14:31:26 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 /* For using exey_ctrl */
 int					nmap_init_exey(T_CLIENT_ROOT *root, t_exe *exe , t_blk *blk);
+int					nmap_clean_exey(T_CLIENT_ROOT *root, t_exe *exe , t_blk *blk);
 
 #endif
